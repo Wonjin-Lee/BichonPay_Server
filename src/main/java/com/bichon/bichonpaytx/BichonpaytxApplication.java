b933caa1717bc7d21252133e9711e819dbf7a94f
Wonjin-Lee <claude17@naver.com>
2021-01-24 23:40:03 +0900

    Initialize BichonPay Project

---
+++ src/main/java/com/bichon/bichonpaytx/BichonpaytxApplication.java
@@ -0,0 +1,13 @@
+package com.bichon.bichonpaytx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BichonpaytxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BichonpaytxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bichon/bichonpaytx/BichonpaytxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bichon.bichonpaytx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BichonpaytxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
