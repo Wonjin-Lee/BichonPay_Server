e9d87c1279e7e28bc039ea1c283e7f9b1a56268c
Wonjin-Lee <claude17@naver.com>
2021-03-28 16:57:49 +0900

    [기능] Session 활용을 위한 라이브러리 추가, UserDto 인스턴스를 세션에 저장할 수 있도록 변경

--- src/main/java/com/bichon/bichonpaytx/controller/SignUpController.java
+++ src/main/java/com/bichon/bichonpaytx/controller/SignUpController.java
@@ -6,39 +6,43 @@ import com.bichon.bichonpaytx.dto.UserDto;
 import com.bichon.bichonpaytx.service.signup.SignUpService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @RestController
 public class SignUpController {
 
+    @Resource
+    private UserDto userInfoSession;
+
     @Autowired
     private SignUpService signUpService;
 
     @RequestMapping(value = "/users/creation/auth", method = RequestMethod.POST)
-    public String authUserInfo(@RequestBody UserDto userDto) {
-        log.debug("NAME : " + userDto.getName());
-        log.debug("PHONE : " + userDto.getPhone());
-        log.debug("BIRTH : " + userDto.getBirth());
-
+    public String authUserInfo(@RequestBody UserDto userDto, HttpSession commonSession) {
         // 1. 필수값 체크
 
         // 2. 가입 여부 확인
-        String result = signUpService.validateUserInfo(userDto);
-        log.debug("Validation Result : " + result);
+        String validationResult = signUpService.validateUserInfo(userDto);
+        log.debug("가입 여부 확인 결과 : " + validationResult);
 
-        switch (result) {
+        switch (validationResult) {
             case StatusConstants.MEMBER_INFO_NOT_EXIST:
                 // 2-1. 미가입자의 경우 - OTP 생성, 세션에 OTP 저장
                 String otp = OTPGenerator.generate(6);
+
+                commonSession.setAttribute("OTP", otp);
 
                 // 3. 문자 전송 요청
 
             case StatusConstants.MEMBER_INFO_MATCHED:
                 // 2-2. 기가입자의 경우 - 세션에 유저 데이터 저장, 응답으로 user_id를 내려줌
+
 
             case StatusConstants.NAME_NOT_EQUAL:
             case StatusConstants.BIRTH_NOT_EQUAL:
@@ -46,5 +50,22 @@ public class SignUpController {
 
         // Response 반환
         return "Result=0&ResultMsg=SUCCESS";
+    }
+
+    @RequestMapping(value = "/users/creation/checkotp", method = RequestMethod.POST)
+    public String checkOtp(@RequestBody Map<String, String> requestMap, HttpSession commonSession) {
+        try {
+            log.debug("Request OTP : " + requestMap.get("otp"));
+            log.debug("Session OTP : " + commonSession.getAttribute("OTP"));
+
+            if (!Objects.equals(requestMap.get("otp"), commonSession.getAttribute("OTP"))) {
+                throw new IllegalArgumentException("OTP가 일치하지 않습니다.");
+            }
+
+            return "Result=0&ResultMsg=SUCCESS";
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
